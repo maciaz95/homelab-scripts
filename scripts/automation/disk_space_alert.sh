c6ff703721eb955b1bba0003baec0d19537720ff
maciaz95 <majkswc@gmail.com>
2025-11-21 15:10:18 +0100

    Refreshed output syntax

--- scripts/automation/disk_space_alert.sh
+++ scripts/automation/disk_space_alert.sh
@@ -12,16 +12,16 @@ USAGE=$(df -h $PARTITION | awk 'NR==2{print $5}' | sed 's/%//')
 
 echo "*** DISK SPACE MONITOR ***"
 echo ""
-echo "* * * Date: $(date)"
-echo "* * * Checking partition: $PARTITION"
-echo "* * * Current usage: ${USAGE}%"
-echo "* * * Alert threshold: ${THRESHOLD}%"
+echo ">>> Date: $(date)"
+echo ">>> Checking partition: $PARTITION"
+echo ">>> Current usage: ${USAGE}%"
+echo ">>> Alert threshold: ${THRESHOLD}%"
 echo ""
 
 if  [ $USAGE -gt $THRESHOLD ]; then
 	echo "!!! ALERT: Disk usage is ${USAGE}% - exceeds threshold!"
 	echo "Free space: $(df -h $PARTITION | awk 'NR==2{print $4}')"
 else
-	echo "*** OK: Disk usage on partition root (/) is within acceptable limits"
+	echo ">>> OK: Disk usage on partition root (/) is within acceptable limits"
 fi
 
